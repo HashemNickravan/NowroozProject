366bc4cbac13c422aec0af4dd22aaaa8ab32e477
Hashem Nickravan <hashem.nickravan@gmail.com>
2025-04-08 07:29:12 +0330

    Update and repair some problems in Entity class

--- main/db/Entity.java
+++ main/db/Entity.java
@@ -2,6 +2,6 @@ package main.db;
 
 public abstract class Entity {
     public int id;
-    public abstract Entity copy();
     public abstract int getEntityCode();
+    public abstract Entity copy();
 }
