43cf8fc92f46533f80d6d78bfd936f50cb13cbd3
Hashem Nickravan <hashem.nickravan@gmail.com>
2025-04-08 07:30:25 +0330

    Update and repair some problems in Trackable class

--- main/db/Trackable.java
+++ main/db/Trackable.java
@@ -4,8 +4,7 @@ import java.util.Date;
 
 public interface Trackable {
     void setCreationDate(Date date);
-    Date getCreationDate();
-
     void setLastModificationDate(Date date);
+    Date getCreationDate();
     Date getLastModificationDate();
 }
