866ac5740d5eb245e74490eb089f2acb09549a06
Hashem Nickravan <hashem.nickravan@gmail.com>
2025-04-08 07:31:15 +0330

    create step class in todo.entity package

--- main/todo/entity/Step.java
+++ main/todo/entity/Step.java
@@ -1,42 +1,35 @@
 package main.todo.entity;
 
-import database.Entity;
+import main.db.Entity;
 
 public class Step extends Entity {
-    public enum Status {
-        NotStarted, Completed
-    }
+    public static final int ENTITY_CODE = 1002;
+
+    public enum Status { NotStarted, Completed }
 
     private String title;
-    private Status status;
+    private Status status = Status.NotStarted;
     private int taskRef;
 
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
-    public Step() {
-        this.status = Status.NotStarted;
-    }
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
 
-    public String getTitle() {
-        return title;
-    }
+    public int getTaskRef() { return taskRef; }
+    public void setTaskRef(int taskRef) { this.taskRef = taskRef; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    @Override
+    public int getEntityCode() { return ENTITY_CODE; }
 
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public int getTaskRef() {
-        return taskRef;
-    }
-
-    public void setTaskRef(int taskRef) {
-        this.taskRef = taskRef;
+    @Override
+    public Entity copy() {
+        Step copy = new Step();
+        copy.id = this.id;
+        copy.title = this.title;
+        copy.status = this.status;
+        copy.taskRef = this.taskRef;
+        return copy;
     }
 }
