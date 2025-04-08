12a70dba1d63c37a57506dc74aa7fe3accadd36a
Hashem Nickravan <hashem.nickravan@gmail.com>
2025-04-08 07:32:09 +0330

    create Task class in todo.entity package

--- main/todo/entity/Task.java
+++ main/todo/entity/Task.java
@@ -1,52 +1,55 @@
 package main.todo.entity;
 
+import main.db.Entity;
+import main.db.Trackable;
 import java.util.Date;
-import database.Entity;
-import todo.Trackable;
 
 public class Task extends Entity implements Trackable {
-    public enum Status {
-        NotStarted, InProgress, Completed
-    }
+    public static final int ENTITY_CODE = 1001;
+
+    public enum Status { NotStarted, InProgress, Completed }
 
     private String title;
     private String description;
     private Date dueDate;
-    private Status status;
+    private Status status = Status.NotStarted;
+    private Date creationDate;
+    private Date lastModificationDate;
 
-    public Task() {
-        this.status = Status.NotStarted;
-    }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public Date getDueDate() { return dueDate; }
+    public void setDueDate(Date dueDate) { this.dueDate = dueDate; }
 
-    public String getDescription() {
-        return description;
-    }
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    @Override
+    public Date getCreationDate() { return creationDate; }
+    @Override
+    public void setCreationDate(Date creationDate) { this.creationDate = creationDate; }
+    @Override
+    public Date getLastModificationDate() { return lastModificationDate; }
+    @Override
+    public void setLastModificationDate(Date lastModificationDate) { this.lastModificationDate = lastModificationDate; }
 
-    public Date getDueDate() {
-        return dueDate;
-    }
+    @Override
+    public int getEntityCode() { return ENTITY_CODE; }
 
-    public void setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
+    @Override
+    public Entity copy() {
+        Task copy = new Task();
+        copy.id = this.id;
+        copy.title = this.title;
+        copy.description = this.description;
+        if (this.dueDate != null) copy.dueDate = new Date(this.dueDate.getTime());
+        copy.status = this.status;
+        if (this.creationDate != null) copy.creationDate = new Date(this.creationDate.getTime());
+        if (this.lastModificationDate != null) copy.lastModificationDate = new Date(this.lastModificationDate.getTime());
+        return copy;
     }
 }
